0aee8708300ba70120b2a994a5bbab49861608cb
TSGameDev <tyler.lazarus01@outlook.com>
2021-08-24 19:03:38 +0100

    20.1 - Start of re-design
    Due to clunkyness and some bugs due to anims and anim events. I have decided to redesign the animator controller structure and make use of state behaviuour scripts.
    I will go on to change the Audio as well to make it easier to impliment, change and fine tune using scriptable objects.

---
+++ Assets/RandomAnim.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAnim : StateMachineBehaviour
+{
+    [Tooltip("The number of states that can be randomly selected.")]
+    [SerializeField] private int numberOfIdleStates;
+    [Tooltip("The name of the int anim variable that is used as the condition for the transitions.")]
+    [SerializeField] private string intStateCounterName;
+    private int stateCounterHash;
+
+    //OnStateMachineEnter is called when entering a state machine via its Entry Node
+    override public void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
+    {
+        stateCounterHash = Animator.StringToHash(intStateCounterName);
+        int RanNum = Random.Range(0, numberOfIdleStates);
+        animator.SetInteger(stateCounterHash, RanNum);
+    }
+}
